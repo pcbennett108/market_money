9e2d25840028a0527839e2179ee5079eec989e50
pcbennett108 <pcbennett42@gmail.com>
2023-12-06 11:11:00 -0700

    Add model assoc. and serializer, controller def for all markets endpoint

---
+++ app/models/vendor.rb
@@ -0,0 +1,8 @@
+class Vendor < ApplicationRecord
+  has_many :market_vendors, dependent: :destroy
+  has_many :markets, through: :market_vendors
+  validates_presence_of :name,
+                        :description,
+                        :contact_name,
+                        :contact_phone
+end
